e078093cace0f9a32f8c2717dd76f06d5c1ff7d7
Hiroki Kondo <kompiro@gmail.com>
2017-01-02 01:22:27 +0900

    Enable to show contributors

--- src/pr-summary.js
+++ src/pr-summary.js
@@ -67,16 +67,39 @@ const fetchPullRequests = (owner, repo, prNumbers) => {
 };
 
 const getPRInfo = (owner, repo, number) => {
-  return getCommitsFromPullRequest(owner, repo, number).
-    then(filterMerged).
-    then(fetchPullRequests.bind(this, owner, repo));
+  return new Promise((resolve) => {
+    const prInfo = {};
+    getCommitsFromPullRequest(owner, repo, number).
+      then((commits) => {
+        prInfo.commits = commits;
+        return commits;
+      }).
+      then(filterMerged).
+      then(fetchPullRequests.bind(this, owner, repo)).
+      then((prs) => {
+        prInfo.prs = prs;
+        resolve(prInfo);
+      });
+  });
 };
 
 const prsummary = (owner, repo, number) => {
   getPRInfo(owner, repo, number).
-    then((prs) => {
-      console.info('## Pull Requests Title');
-      prs.map((pr) => {
+    then((prInfo) => {
+      console.info('## Contributors\n');
+      const rendered = [];
+      prInfo.commits.map((commit) => {
+        const user = commit.author.login;
+        if (rendered.includes(user) === false && user.trim() !== '') {
+          console.info(user);
+          rendered.push(user);
+        }
+      });
+
+      console.info('');
+
+      console.info('## Pull Requests\n');
+      prInfo.prs.map((pr) => {
         if (pr.pull_request) {
           const message = `#${pr.number} ${pr.title} by ${pr.user.login}`;
           console.info(message);
