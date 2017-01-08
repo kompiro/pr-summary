8039695757be23d60f13f69357233701afc91cfc
Hiroki Kondo <kompiro@gmail.com>
2017-01-08 21:11:11 +0900

    Define spec of paging

--- test/github_client_test.js
+++ test/github_client_test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const context = require('mocha').describe;
+
 import assert from 'power-assert';
 import nock from 'nock';
 
@@ -27,20 +29,51 @@ describe('GitHubClient', () => {
 
   describe('#getCommitsFromPullRequest', () => {
 
-    nock('https://api.github.com/').
-      get('/repos/kompiro/awesome-app/pulls/3/commits').
+    const ENTRY_POINT_OF_PR_COMMITS = '/repos/kompiro/awesome-app/pulls/3/commits';
+
+    context('no paging', () => {
+      nock('https://api.github.com/').
+      get(ENTRY_POINT_OF_PR_COMMITS).
       reply(200, [
         { sha: 'sha0' },
         { sha: 'sha1' }
       ]);
 
-    it ('returns commits', (done) => {
-      sut.getCommitsFromPullRequest('kompiro', 'awesome-app', 3).then((commits) => {
-        assert(commits.length === 2);
-        assert(commits[0].sha === 'sha0');
-        done();
-      }).catch(done);
+      it ('returns commits', (done) => {
+        sut.getCommitsFromPullRequest('kompiro', 'awesome-app', 3).then((commits) => {
+          assert(commits.length === 2);
+          assert(commits[0].sha === 'sha0');
+          done();
+        }).catch(done);
+      });
     });
+
+    context('have paging', () => {
+      nock('https://api.github.com/').
+      get(ENTRY_POINT_OF_PR_COMMITS).
+      reply(200, [
+        { sha: 'sha0' },
+        { sha: 'sha1' }
+      ],{
+        Link: `<https://api.github.com${ENTRY_POINT_OF_PR_COMMITS}?page=2>; rel="next",` +
+        `<https://api.github.com${ENTRY_POINT_OF_PR_COMMITS}?page=2>; rel="last"`
+      }).
+      get(ENTRY_POINT_OF_PR_COMMITS).
+      query({page: 2}).
+      reply(200, [
+        { sha: 'sha2' },
+        { sha: 'sha3' }
+      ]);
+
+      it ('returns commits', (done) => {
+        sut.getCommitsFromPullRequest('kompiro', 'awesome-app', 3).then((commits) => {
+          assert(commits.length === 4);
+          assert(commits[2].sha === 'sha2');
+          done();
+        }).catch(done);
+      });
+    });
+
   });
 
 });
