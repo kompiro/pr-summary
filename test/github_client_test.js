f595b72645e9e655bc4759d974b91af38445e3c6
Hiroki Kondo <kompiro@gmail.com>
2017-01-08 20:57:45 +0900

    Define #getCommitsFromPullRequest

--- test/github_client_test.js
+++ test/github_client_test.js
@@ -1,12 +1,14 @@
 'use strict';
 
 import assert from 'power-assert';
+import nock from 'nock';
 
 import GitHubAPI from 'github';
 import {GitHubClient} from '../lib/github_client';
 
 let sut;
 describe('GitHubClient', () => {
+
   beforeEach(() => {
     const api = new GitHubAPI();
 
@@ -22,4 +24,23 @@ describe('GitHubClient', () => {
     assert(sut.client.auth.type === 'token');
     assert(sut.client.auth.token === 'GITHUB_TOKEN');
   });
+
+  describe('#getCommitsFromPullRequest', () => {
+
+    nock('https://api.github.com/').
+      get('/repos/kompiro/awesome-app/pulls/3/commits').
+      reply(200, [
+        { sha: 'sha0' },
+        { sha: 'sha1' }
+      ]);
+
+    it ('returns commits', (done) => {
+      sut.getCommitsFromPullRequest('kompiro', 'awesome-app', 3).then((commits) => {
+        assert(commits.length === 2);
+        assert(commits[0].sha === 'sha0');
+        done();
+      }).catch(done);
+    });
+  });
+
 });
