4c81cf1855bc680086f1ac82119b74271f109a19
Hiroki Kondo <kompiro@gmail.com>
2017-01-09 22:41:29 +0900

    Define #prepareRelease

--- test/github_client_test.js
+++ test/github_client_test.js
@@ -159,4 +159,88 @@ describe('GitHubClient', () => {
 
   });
 
+  describe('#prepareRelease', () => {
+
+    context('pull request is already exist', () => {
+      beforeEach(() => {
+        nock(ENTRY_POINT_OF_GITHUB).
+        post(ENTRY_POINT_OF_PULL_REQUESTS, {
+          title: 'Prepare to deploy develop to master',
+          head: 'develop',
+          base: 'master'
+        }).
+        reply(422, {
+          message: 'Validation Failed',
+          errors: [
+            {
+              resource: 'PullRequest',
+              code: 'custom',
+              message: 'A pull request already exists for kompiro.master.'
+            }
+          ],
+          documentation_url: 'https://developer.github.com/v3/pulls/#create-a-pull-request'
+        });
+      });
+
+      it ('cannot create pull request', (done) => {
+        sut.prepareRelease('kompiro', 'awesome-app', 'master', 'develop').catch((error)=>{
+          assert(error);
+          done();
+        });
+      });
+    });
+
+    context('succeed to create pull request', () => {
+      beforeEach(() => {
+        nock(ENTRY_POINT_OF_GITHUB).
+        post(ENTRY_POINT_OF_PULL_REQUESTS, {
+          title: 'Prepare to deploy develop to master',
+          head: 'develop',
+          base: 'master'
+        }).
+        reply(201, {
+          number: 3,
+          title: 'Prepare to deploy develop to master'
+        }).
+        get(ENTRY_POINT_OF_PR_COMMITS).
+        reply(200, [
+          { sha: 'sha0', author: {login: 'kompiro'} },
+          { sha: 'sha1', author: {login: 'kompiro'} }
+        ],{
+          Link: `<https://api.github.com${ENTRY_POINT_OF_PR_COMMITS}?page=2>; rel="next",` +
+          `<https://api.github.com${ENTRY_POINT_OF_PR_COMMITS}?page=2>; rel="last"`
+        }).
+        get(ENTRY_POINT_OF_PR_COMMITS).
+        query({page: 2}).
+        reply(200, [
+          { sha: 'sha2', author: {login: 'kompiro'}  },
+          { sha: 'sha3', author: {login: 'kompiro'}  }
+        ]).
+        get(ENTRY_POINT_OF_PULL_REQUESTS).
+        query(true).
+        reply(200, [
+          { number: 1, head: {sha: 'sha0' } ,merged_at: '2017-01-08T21:56:36+09:00' },
+          { number: 2, head: {sha: 'sha3' } ,merged_at: '2017-01-08T23:56:36+09:00' }
+        ]).
+        intercept(`${ENTRY_POINT_OF_PULL_REQUESTS}/3`, 'PATCH').
+        reply(200, {
+          number: 3,
+          title: 'Prepare to deploy develop to master',
+          body: 'updated'
+        });
+      });
+
+      it ('success to create pull request', () => {
+        return sut.prepareRelease('kompiro', 'awesome-app', 'master', 'develop').then((pr) => {
+          assert.equal(pr.number, 3);
+          assert.equal(pr.body, 'updated');
+          assert.equal(pr.title, 'Prepare to deploy develop to master');
+          assert.equal(pr.commits.length, 4);
+          assert.equal(pr.contributors.length, 1);
+        });
+      });
+    });
+
+  });
+
 });
